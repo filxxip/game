7e2a1f4bed373477f3b84268b432ca6adecd749e
Filip Poltoraczyk <f.poltoraczyk@gmail.com>
2021-10-30 16:28:21 +0200

    TIMEgit add -Agit add -A

--- labirynt.py
+++ labirynt.py
@@ -1,5 +1,13 @@
 board_index = []
 import random
+from datetime import datetime
+from getkey import getkey, keys
+import copy
+import time
+import os
+from threading import Thread
+def cleaar():
+    os.system('cls' if os.name == 'nt' else 'clear')
 def board_create():
     for x in range(0, 40):
         board_index.append([])
@@ -13,7 +21,7 @@ def board_create():
         board_index[0][x] = "_"
         board_index[39][x] = "_"
     board_index[random.randrange(1, 39)][random.randrange(1, 64)] = "x"
-    for x in range (0, 1500):
+    for x in range (0, 1200):
         number1 = random.randrange(1, 39)
         number2 = random.randrange(1, 64)
         while board_index[number1][number2] != " ":
@@ -44,6 +52,24 @@ def board_create():
                 elif number3 == 2:
                     if board_index[x][y+1]=="#": board_index[x][y+1] = " "
                     else: board_index[x][y-1] = " "
+    for x in range (0, 10):
+        number1 = random.randrange(1, 39)
+        number2 = random.randrange(1, 64)
+        while board_index[number1][number2] in( "#", "x", "T"):
+            number1 = random.randrange(1, 39)
+            number2 = random.randrange(1, 64)
+        board_index[number1][number2] = "$"
+    for x in range (0, 7):
+        number1 = random.randrange(1, 39)
+        number2 = random.randrange(1, 64)
+        while board_index[number1][number2] in( "#", "x", "$"):
+            number1 = random.randrange(1, 39)
+            number2 = random.randrange(1, 64)
+        board_index[number1][number2] = "T"
+    start = datetime.now()
+    global time1
+    time1 = start.hour*60*60 + start.minute*60 + start.second + start.microsecond/1000000
+    
 
 
 def get_board():
@@ -52,8 +78,68 @@ def get_board():
             print(board_index[x][y], end ="")
         print("")
     
+def b_m():
+    global key
+    key = None
+    while True:
+        key = getkey()
+        time.sleep(0.08)
+        if key == "q":
+            quit()
 
 
+def board_move():
+    board_index2 = copy.deepcopy(board_index)
+    for x in range(1, 39): 
+        for y in range(1, 64):
+            if board_index2[x][y] == "x": 
+                board_index2[x][y] = " "
+                coordinates = [x, y]
+    click = True
+    while click:
+        cleaar()
+        get_board()
+        now = datetime.now()
+        time2 = now.hour*60*60 + now.minute*60 + now.second + now.microsecond/1000000
+        print("Game time:",  300 - round(time2-time1, 1))
+        
+        time.sleep(0.08)
+        global key
+        if key == "w":
+            coordinates[0]-=1
+            if 39>coordinates[0] >0 and 64>coordinates[1] >0 and board_index[coordinates[0]][coordinates[1]] not in ("#"):
+                board_index[coordinates[0]][coordinates[1]] = "x"
+                board_index[coordinates[0]+1][coordinates[1]] = board_index2[coordinates[0]+1][coordinates[1]]
+            else: coordinates[0]+=1
+        elif key == "s":
+            coordinates[0]+=1
+            if 39>coordinates[0] >0 and 64>coordinates[1] >0 and board_index[coordinates[0]][coordinates[1]] not in ("#"):
+                board_index[coordinates[0]][coordinates[1]] = "x"
+                board_index[coordinates[0]-1][coordinates[1]] = board_index2[coordinates[0]-1][coordinates[1]]
+            else: coordinates[0]-=1
+        elif key == "a":
+            coordinates[1]-=1
+            if 39>coordinates[0] >0 and 64>coordinates[1] >0 and board_index[coordinates[0]][coordinates[1]] not in ("#"):
+                board_index[coordinates[0]][coordinates[1]] = "x"
+                board_index[coordinates[0]][coordinates[1]+1] =  board_index2[coordinates[0]][coordinates[1]+1]
+            else: coordinates[1]+=1
+        elif key == "d":
+            coordinates[1]+=1
+            if 39>coordinates[0] >0 and 64>coordinates[1] >0 and board_index[coordinates[0]][coordinates[1]] not in ("#"):
+                board_index[coordinates[0]][coordinates[1]] = "x"
+                board_index[coordinates[0]][coordinates[1]-1] = board_index2[coordinates[0]][coordinates[1]-1]
+            else: coordinates[1]-=1
+        elif key == "q":
+            exit()
+        if board_index2[coordinates[0]][coordinates[1]] in ("$", "T"):
+            board_index2[coordinates[0]][coordinates[1]] = " "
+        
+        key = None
+
+def money_getter():
+    number = random.randrange(0, 1001)
+    #users[index].set_money(users[index].get_money()+number)
 board_create()
-get_board()
-
+if __name__ == '__main__':
+    Thread(target = board_move).start()
+    Thread(target = b_m).start()
